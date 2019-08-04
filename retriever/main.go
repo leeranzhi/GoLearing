5051cc3af44dcb9a5bf6dd172bec73c9a4d3fae2
leeranzhi <2804581329@qq.com>
2019-08-04 23:45:02 +0800

    接口的组合.

--- retriever/main.go
+++ retriever/main.go
@@ -12,6 +12,34 @@ type Retriever interface {
 	Get(url string) string
 }
 
+type Poster interface {
+	Post(url string,
+		form map[string]string) string
+}
+
+const url = "http://www.imooc.com"
+
+func post(poster Poster) {
+	poster.Post(url,
+		map[string]string{
+			"name":   "ccmouse",
+			"course": "golang",
+		})
+}
+
+//接口的组合
+type RetrieverPoster interface {
+	Retriever
+	Poster
+}
+
+func session(s RetrieverPoster) string {
+	s.Post(url, map[string]string{
+		"contents": "another faked imooc.com",
+	})
+	return s.Get(url)
+}
+
 //接口是隐式的
 func download(r Retriever) string {
 	return r.Get("http://www.imooc.com")
@@ -19,7 +47,8 @@ func download(r Retriever) string {
 
 func main() {
 	var r Retriever
-	r = mock.Retriever{"this is a fake immoc.com"}
+	retriever := mock.Retriever{"this is a fake immoc.com"}
+	r = &retriever
 	inspect(r)
 
 	r = &real2.Retriever{
@@ -34,6 +63,9 @@ func main() {
 		fmt.Println("not a mock retriever")
 	}
 
+	fmt.Println("Try a seesion")
+	fmt.Println(session(&retriever))
+
 	//fmt.Println(download(r))
 }
 
@@ -42,7 +74,7 @@ func inspect(retriever Retriever) {
 	fmt.Printf("%T %v\n", retriever, retriever)
 
 	switch v := retriever.(type) {
-	case mock.Retriever:
+	case *mock.Retriever:
 		fmt.Println("Contents:", v.Contents)
 	case *real2.Retriever:
 		fmt.Println("UserAgent:", v.UserAgent)
